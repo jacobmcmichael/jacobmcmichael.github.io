def652847c230a61f1c8db40e27c0e71c31880eb
jacobmcmichael <jacobmcmichael@gmail.com>
2024-08-26 15:14:29 -0700

    :building_construction: cleans up project config files

---
+++ src/components/HomeLayout.jsx
@@ -0,0 +1,11 @@
+/* Dependencies */
+import React from "react";
+
+/* Stylesheets */
+import "@/src/styles/_reset.css";
+import "@/src/styles/_variables.css";
+import "@/src/styles/_global.css";
+
+export default function HomeLayout({ children }) {
+  return <>{children}</>;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+/* Dependencies */
+import React from "react";
+
+/* Layouts */
+import HomeLayout from "@/src/components/HomeLayout";
+import HeroSection from "@/src/components/HeroSection";
+
+export default function HomePage() {
+  return (
+    <HomeLayout>
+      <HeroSection />
+    </HomeLayout>
+  );
+}
