3aff8dbc03a3f1eaedbb26a8c7281b407444ed3e
jacobmcmichael <jacobmcmichael@gmail.com>
2024-08-29 16:35:11 -0700

    :lipstick: adds icons and styling to the social links

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,13 @@
 /* Dependencies */
 import React from "react";
 import Button from "@/components/Button";
-import { ArrowDown, ArrowUpRight } from "@/components/Icons";
+import {
+  ArrowDown,
+  ArrowUpRight,
+  Google,
+  LinkedIn,
+  GitHub,
+} from "@/components/Icons";
 
 /* Stylesheets */
 import "@/styles/hero.css";
@@ -23,14 +29,41 @@ export default function Hero() {
       <div className="hero__bottom">
         <div className="bottom__contact">
           <h2 className="h5 title">Let's Talk</h2>
-          <a href="mailto:jacobmcmichael@gmail.com" target="_blank">
-            jacobmcmichael<wbr></wbr>
-            <span>
-              @gmail.com
-              <ArrowUpRight />
-            </span>
-          </a>
+          <ul className="contact__links">
+            <li>
+              <a href="mailto:jacobmcmichael@gmail.com" target="_blank">
+                <Google />
+                jacobmcmichael<wbr></wbr>
+                <span>
+                  @gmail.com
+                  <ArrowUpRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/jacobmcmichael/"
+                target="_blank"
+              >
+                <LinkedIn />
+                <span>
+                  jacobmcmichael
+                  <ArrowUpRight />
+                </span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/jacobmcmichael" target="_blank">
+                <GitHub />
+                <span>
+                  jacobmcmichael
+                  <ArrowUpRight />
+                </span>
+              </a>
+            </li>
+          </ul>
         </div>
+
         <p className="bottom__message">
           Nice to meet you! I'm Jake, a Frontend Developer with experience in
           e-commerce, web development, and scalable applications.
