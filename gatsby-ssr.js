a02f1eb6018bc1a668fbbce680393ec48c8582b2
jacobmcmichael <jacobmcmichael@gmail.com>
2024-09-09 18:51:36 -0700

    :feat: populates meta description; refactors font loading logic

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,26 @@
-exports.onRenderBody = ({ setBodyAttributes }) => {
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents, setBodyAttributes }) => {
+  setHeadComponents([
+    <link
+      key="google-fonts-preconnect"
+      rel="preconnect"
+      href="https://fonts.googleapis.com"
+    />,
+    <link
+      key="google-fonts-preconnect-gstatic"
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="google-fonts-stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&family=Unbounded:wght@200..900&display=swap"
+      rel="stylesheet"
+    />,
+  ]);
+
   setBodyAttributes({
-    "data-theme": 'dark', // Set the default theme property
+    "data-theme": "dark", // Keep your existing theme setting
   });
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,20 +22,9 @@ export function Head() {
   return (
     <>
       <title>Jacob McMichael</title>
-      {`TODO: Update Meta info`}
       <meta
         name="description"
-        content="Welcome to the home page of My Awesome SPA"
-      />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&family=Unbounded:wght@200..900&display=swap"
-        rel="stylesheet"
+        content="I'm a web developer passionate about crafting digital experiences that captivate users and drive growth. With expertise in e-commerce, web design, data analysis, and marketing strategy, I blend creativity and technical skills to drive client success and customer engagement."
       />
     </>
   );
